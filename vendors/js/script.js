3dafb3fe8d02106da0fedd6baf65ad93a4cdcc8f
subhom11 <subhomkundu@gmail.com>
2019-08-24 18:34:21 +0530

    missing files added

---
+++ vendors/js/script.js
@@ -0,0 +1,25 @@
+// Navigate to contents
+
+$(document).ready(function () {
+
+    /* For the sticky navigation */
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+            offset: '60px;'
+        });
+
+
+    /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({ scrollTop: $('.js--section-plans').offset().top }, 1000);
+    });
+
+
+
+
+});
